b549ea37f562d1d027f58654f6487fd991285eb1
Sujeet Sharma <sksharmacs2005@gmail.com>
2024-11-05 11:43:39 +0530

    unwanted import package remove

--- src/main/java/com/java/sk/model/Employee.java
+++ src/main/java/com/java/sk/model/Employee.java
@@ -2,14 +2,12 @@ package com.java.sk.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Document(collection = "empinfo")
 public class Employee {
     @Id
     private Long empId;
