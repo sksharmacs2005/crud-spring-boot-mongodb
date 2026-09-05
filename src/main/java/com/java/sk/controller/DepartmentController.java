40960d363ac2b9d9b51bf327c6de107f2648a97c
Sujeet Sharma <sksharmacs2005@gmail.com>
2026-09-05 20:26:21 +0530

    department class commit again

--- src/main/java/com/java/sk/controller/DepartmentController.java
+++ src/main/java/com/java/sk/controller/DepartmentController.java
@@ -1,5 +1,9 @@
 package com.java.sk.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DepartmentController {
     //This is the department controller class
+    private static final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
 }
