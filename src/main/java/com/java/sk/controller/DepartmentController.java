9b32359f59cae9db84137a613721908e098cfae2
Sujeet Sharma <sksharmacs2005@gmail.com>
2026-09-05 20:39:58 +0530

    department class add the constructor parameter

--- src/main/java/com/java/sk/controller/DepartmentController.java
+++ src/main/java/com/java/sk/controller/DepartmentController.java
@@ -6,4 +6,10 @@ import org.slf4j.LoggerFactory;
 public class DepartmentController {
     //This is the department controller class
     private static final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+    String name;
+    String location;
+
+    public DepartmentController(String name, String location) {
+    }
+
 }
